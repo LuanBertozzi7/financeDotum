f67be3ecea8151700d4070d2d1cea42f0f6c9c62
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-11-16 22:50:46 -0400

    test: uploading project at vercel

--- api/server.js
+++ api/server.js
@@ -1,20 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+
+// Constrói o caminho para a pasta 'public' de forma segura
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, "../public");
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} - IP: ${req.ip}`);
   next();
 });
 
-app.use(express.static("public"));
+app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-  console.log(`${req.ip} to /index.js`);
   res.redirect("/index.html");
 });
 
-app.listen(PORT);
 export default app;
